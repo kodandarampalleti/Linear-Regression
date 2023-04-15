c6d12f87c2328dda283b6242ee674ddc62fe99c6
kodandarampalleti <palletikodandaram@gmail.com>
2023-04-15 15:57:44 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import find_packages , setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readline()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -15,12 +15,11 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 
-
 setup(
-    name='RegressionProject' , 
-    version= '0.0.1',
-    author='Alwayss Ram' ,
-    author_email='alwayssram@gmail.com',
-    install_requires= get_requirements('requirements.txt'), 
+    name='RegressorProject',
+    version='0.0.1',
+    author='Krissh',
+    author_email='krish.naik@ineuron.ai',
+    install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 )
